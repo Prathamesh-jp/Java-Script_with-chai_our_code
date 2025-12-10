1b8b570071e1bb72bc84f447d35a537b64561cf6
Prathamesh Mhettar <prathmeshmhettar@gmail.com>
2025-12-10 14:51:39 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const acountId = 12345
+let accountEmail = "prathamesh@gmail.com"
+var accountpassward = "12345"
+accountCity = "kolhapur"
+
+accountEmail = "mahesh@123"
+accountpassward = "123455"
+let accountState;
+
+console.table([accountEmail,accountpassward,acountId,accountState]);
+// acountId = 12 not allowed in java script that we can not assignment to constant variables
+/*
+prefer to use and let 
+not use var beacause the var can declare by globaly and the let is used in block only we can 
+also {} in this type of bracket the let will be change after that complition of block
+*/
+
+console.log(acountId);
