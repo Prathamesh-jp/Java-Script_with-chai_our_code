45761640d21cffe40c718132cbe59c363a61644c
Prathamesh Mhettar <prathmeshmhettar@gmail.com>
2025-12-13 13:52:24 +0000

    study about memory

--- 01_basics/dataypessummary.js
+++ 01_basics/dataypessummary.js
@@ -12,7 +12,7 @@ let userEmail;
 const id = Symbol('123')
 const anotherId = Symbol('123')
 
-console.log(id === anotherId);// not same symbol always different 
+// console.log(id === anotherId);// not same symbol always different 
 
 // const bigNumber = 3456543576654356754n
 
@@ -29,9 +29,35 @@ let myObj = {
 }
 
 const myFunction = function(){
-    console.log("Hello world");
+    // console.log("Hello world");
 }
 
-console.log(typeof anotherId);
+// console.log(typeof anotherId);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+// **************************************/ Memory spcae allocation  /*************************************************
+
+// stack (Premitive) and heap (non-Premitive) memory
+
+let youtubeName = "hitesh"
+
+let anotherName = youtubeName
+
+anotherName = "mahesh"
+
+console.log(youtubeName);
+console.log(anotherName);
+
+let person1 = {
+    "name" : "prathamesh",
+    "age" : 25
+} 
+
+let person2 = person1;
+
+person2.age = 45;
+
+console.log(person1);
+
+console.log(person2);
